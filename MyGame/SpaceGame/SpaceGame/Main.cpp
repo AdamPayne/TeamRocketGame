48a46584639b8e963a5e21cfd0ddaf20d65d4151
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2015-07-16 19:17:55 +0300

    First Commit. Basic Movement Added

--- MyGame/SpaceGame/SpaceGame/Main.cpp
+++ MyGame/SpaceGame/SpaceGame/Main.cpp
@@ -1,11 +1,13 @@
-/*****************************************************************************************
-* Desc: Tutorial 02 IND_Surface
-*****************************************************************************************/
-
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <ctime>
 #include "CIndieLib.h"
 #include "IND_Surface.h"
 #include "IND_Entity2d.h"
 #include "IND_Animation.h"
+#include "IND_Font.h"
+#include "Planet.h"
+#include "Ship.h"
 
 /*
 ==================
@@ -14,89 +16,48 @@ Main
 */
 int IndieLib()
 {
-	
 	// ----- IndieLib intialization -----
 
 	CIndieLib *mI = CIndieLib::instance();
 	if (!mI->init()) return 0;
-
 	
+	// ----- Get Window Dimensions
+
+	int winWidth = mI->_window->getWidth();
+	int winHeight = mI->_window->getHeight();
+
 	// ----- Surface loading -----
 
-	// Loading Background
 	IND_Surface *mSurfaceBack = IND_Surface::newSurface();
-	if (!mI->_surfaceManager->add(mSurfaceBack, "../SpaceGame/resources/blue_background.jpg", IND_OPAQUE, IND_32)) return 0;
+	if (!mI->_surfaceManager->add(mSurfaceBack, "../SpaceGame/resources/Backgrounds/18.jpg", IND_OPAQUE, IND_32)) return 0;
 
-	// Loading sprite of a star
-	IND_Surface *mSurfaceStar = IND_Surface::newSurface();
-	if (!mI->_surfaceManager->add(mSurfaceStar, "../SpaceGame/resources/star.png", IND_ALPHA, IND_32)) return 0;
+	IND_Font* mFont = IND_Font::newFont();
+	if (!mI->_fontManager->add(mFont, "../SpaceGame/resources/font_big.png", "../SpaceGame/resources/font_big.xml", IND_ALPHA, IND_32)) return 0;
 
-	// ----- Animations loading -----
+	// Loding 2D Entities
 
-	// Characters animations, we apply a color key of (0, 48, 152)
-	IND_Animation *mAnimationCharacter1 = IND_Animation::newAnimation();
-	if (!mI->_animationManager->addToSurface(mAnimationCharacter1, "../SpaceGame/resources/animations/character1.xml", IND_ALPHA, IND_32, 0, 48, 152)) return 0;
+	IND_Entity2d* mBack = IND_Entity2d::newEntity2d();
+	mI->_entity2dManager->add(mBack);
+	mBack->setSurface(mSurfaceBack);
+	mBack->setScale((float)winWidth / mSurfaceBack->getWidth(), (float)winHeight / mSurfaceBack->getHeight());
 
-	// Characters animations, we apply a color key of (0, 48, 152)
-	IND_Animation *mAnimationCharacter2 = IND_Animation::newAnimation();
-	if (!mI->_animationManager->addToSurface(mAnimationCharacter2, "../SpaceGame/resources/animations/character2.xml", IND_ALPHA, IND_32, 0, 48, 152)) return 0;
+	IND_Entity2d* mText = IND_Entity2d::newEntity2d();
+	mI->_entity2dManager->add(mText);
+	mText->setFont(mFont);
+	mText->setPosition(1366, 200, 0);
+	//mText->setText("");
 
-	// Dust animation, we apply a color key of (255, 0, 255)
-	IND_Animation *mAnimationDust = IND_Animation::newAnimation();
-	if (!mI->_animationManager->addToSurface(mAnimationDust, "../SpaceGame/resources/animations/dust.xml", IND_ALPHA, IND_16, 255, 0, 255)) return 0;
+	Planet* mPlanet = new Planet();
+	mPlanet->createPlanet(mI, "../SpaceGame/resources/planets/1.png", 100, 100, 0.5f, 0.5f);
 
-	// ----- Set the surface and animations into 2d entities -----
-
-	// Creating 2d entity for the background
-	IND_Entity2d *mBack = IND_Entity2d::newEntity2d();
-	mI->_entity2dManager->add(mBack);					// Entity adding
-	mBack->setSurface(mSurfaceBack);					// Set the surface into the entity
-
-	//star
-	IND_Entity2d *star = IND_Entity2d::newEntity2d();
-	mI->_entity2dManager->add(star);
-	star->setSurface(mSurfaceStar);
-
-	// Character 1
-	IND_Entity2d *mPlayer1 = IND_Entity2d::newEntity2d();
-	mI->_entity2dManager->add(mPlayer1);					// Entity adding
-	mPlayer1->setAnimation(mAnimationCharacter1);				// Set the animation into the entity
-
-	// Character 2
-	IND_Entity2d *mPlayer2 = IND_Entity2d::newEntity2d();
-	mI->_entity2dManager->add(mPlayer2);					// Entity adding
-	mPlayer2->setAnimation(mAnimationCharacter2);				// Set the animation into the entity
-
-	// Dust explosion
-	IND_Entity2d *mDust = IND_Entity2d::newEntity2d();
-	mI->_entity2dManager->add(mDust);					// Entity adding
-	mDust->setAnimation(mAnimationDust);					// Set the animation into the entity
-
-	// ----- Changing the attributes of the 2d entities -----
-
-	// Player 1
-	mPlayer1->setSequence(0);						// Choose sequence
-	mPlayer1->setPosition(140, 200, 0);
-	mPlayer1->setMirrorX(1);						// Horizontal mirroring
-
-	// Dust explosion
-	mDust->setPosition(360, 250, 0);
-
-	// Player 2
-	mPlayer2->setSequence(0);						// Choose sequence
-	mPlayer2->setPosition(550, 200, 0);
-	mPlayer2->setNumReplays(3);						// The animation will be displayed 3 times
-
-
-	//star
-	star->setPosition(mI->_window->getWidth() - mSurfaceStar->getWidth(), 0, 100);
-	// ----- Main Loop -----
+	Ship* mShip = new Ship();
+	mShip->createShip(mI, "../SpaceGame/resources/Spaceship with motor new/Ship_advance.xml", winWidth/2, winHeight/2, 0.1f, 0.1f);
 
 	while (!mI->_input->onKeyPress(IND_ESCAPE) && !mI->_input->quit())
 	{
+		mShip->moveShip();
+		//mI->_render->showFpsInWindowTitle();
 		mI->_input->update();
-		// Toogle full screen when pressing "space"
-		//if (mI->_input->onKeyPress(IND_SPACE)) mI->_render->toggleFullScreen();
 		mI->_render->beginScene();
 		mI->_entity2dManager->renderEntities2d();
 		mI->_render->endScene();
